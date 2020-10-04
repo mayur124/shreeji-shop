dc17d7366a66fc318c9e574b2a22af4d983f0086
mayur124 <mayur.chawda@somaiya.edu>
2020-10-04 18:32:10 +0530

    updated transaction pojo and added wishlist pojo

---
+++ Backend/src/main/java/com/shreejiShop/demo/model/Wishlist.java
@@ -0,0 +1,38 @@
+package com.shreejiShop.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "WISHLIST")
+public class Wishlist {
+	
+	@Column(name = "USER_ID")
+	private Long userId;
+
+	@Column(name = "MODEL_ID")
+	private Long modelId;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getModelId() {
+		return modelId;
+	}
+
+	public void setModelId(Long modelId) {
+		this.modelId = modelId;
+	}
+
+	@Override
+	public String toString() {
+		return "Wishlist [userId=" + userId + ", modelId=" + modelId + "]";
+	}
+
+}
