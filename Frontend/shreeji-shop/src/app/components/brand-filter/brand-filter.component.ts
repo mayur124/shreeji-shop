bfa53f054060f6b405d38dd54ec0b79aa04e3f13
mayur124 <mayur.chawda@somaiya.edu>
2020-10-24 20:06:59 +0530

    Hover changes

--- Frontend/shreeji-shop/src/app/components/brand-filter/brand-filter.component.ts
+++ Frontend/shreeji-shop/src/app/components/brand-filter/brand-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { HttpService } from '../../services/http/http.service';
 import { Brand } from './brand.model';
 
@@ -12,10 +13,13 @@ export class BrandFilterComponent implements OnInit {
   brandsCopy: Brand[] = new Array();
   searchClicked: boolean = false;
   moreBrandsClicked: boolean = false;
-  brandsMap: { [letter: string]: string[] } = {};
+  brandsMap: { [letter: string]: Brand[] } = {};
+  brandsMapCopy: { [letter: string]: Brand[] } = {};
   selectedBrands: number[];
 
   @ViewChild('moreBrandsContainer') moreBrandsContainer: ElementRef;
+  @ViewChildren('brandLetter') brandLetter: QueryList<ElementRef>
+  @ViewChildren('brandCb') brandCb: QueryList<ElementRef>
 
   constructor(
     private http: HttpService,
@@ -38,6 +42,7 @@ export class BrandFilterComponent implements OnInit {
         this.brands = response.brands;
         this.brandsCopy = response.brands;
         this.brandsMap = this.getBrandsMap(response.brands);
+        this.brandsMapCopy = this.getBrandsMap(response.brands);
       },
       (error) => {
         console.log('Error while getting all brands > ', error);
@@ -71,6 +76,52 @@ export class BrandFilterComponent implements OnInit {
           .toLowerCase()
           .indexOf((event.target as HTMLInputElement).value.toLowerCase()) > -1
     );
+  }
+
+  filterMoreBrands(event: KeyboardEvent) {
+    this.brandsMapCopy = {};
+    for (const i in this.brandsMap) {
+      for (let j = 0; j < this.brandsMap[i].length; j++) {
+        let filteredBrands = this.brandsMap[i].filter(b =>
+          b.name.toLowerCase().indexOf((event.target as HTMLInputElement).value.toLowerCase()) > -1
+        );
+        if (filteredBrands.length > 0) {
+          this.brandsMapCopy[i] = filteredBrands;
+        }
+      }
+    }
+  }
+
+  scrollTo(index: number) {
+    const el = this.brandLetter.find(bl => bl.nativeElement.id == 'brandId-' + index);
+    const containerCoordinates = (this.moreBrandsContainer.nativeElement as HTMLElement).getBoundingClientRect();
+    const x = (el.nativeElement as HTMLElement).getBoundingClientRect().x;
+    const y = (el.nativeElement as HTMLElement).getBoundingClientRect().y;
+    const scrollX = x - containerCoordinates.x - 50;
+    const scrollY = y - containerCoordinates.y;
+    (this.moreBrandsContainer.nativeElement as HTMLElement).scrollBy(scrollX, scrollY);
+  }
+
+  reduceOpacity(index: number, key: string) {
+    this.brandLetter.forEach(bl => {
+      if (bl.nativeElement.id != 'brandId-' + index) {
+        (bl.nativeElement as HTMLElement).style.opacity = '50%';
+      }
+    });
+    this.brandCb.forEach(bCb => {
+      if (bCb.nativeElement.id != 'brandCb-' + key) {
+        (bCb.nativeElement as HTMLElement).style.opacity = '50%';
+      }
+    });
+  }
+
+  resetOpacity() {
+    this.brandLetter.forEach(bl => {
+      (bl.nativeElement as HTMLElement).style.opacity = '100%';
+    });
+    this.brandCb.forEach(bCb => {
+      (bCb.nativeElement as HTMLElement).style.opacity = '100%';
+    });
   }
 
   resetBrands() {
--- Frontend/shreeji-shop/src/app/components/brand-filter/brand.model.ts
+++ Frontend/shreeji-shop/src/app/components/brand-filter/brand.model.ts
@@ -2,4 +2,8 @@ export class Brand {
     id: number;
     name: string;
     count: number;
+    checked: boolean;
+    constructor() {
+        this.checked = false;
+    }
 }
