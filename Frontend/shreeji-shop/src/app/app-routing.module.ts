0d15c857660eb662cd76375ea3bacab6152bdebe
mayur124 <mayur.chawda@somaiya.edu>
2020-12-05 18:16:01 +0530

    login component changes

--- Frontend/shreeji-shop/src/app/app-routing.module.ts
+++ Frontend/shreeji-shop/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { PhoneDetailComponent } from './components/phone-detail/phone-detail.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'filter', component: HomeComponent },
-  { path: 'search', component: HomeComponent },
-  { path: 'phone/:brandName/:brandId/:id', component: PhoneDetailComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'login', component: LoginComponent }
+    ]
+  },
+  { path: 'phone/:brandName/:brandId/:id', component: PhoneDetailComponent },
 ];
 
 @NgModule({
--- Frontend/shreeji-shop/src/app/components/home/home.module.ts
+++ Frontend/shreeji-shop/src/app/components/home/home.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent } from './pagination/pagination.component';
+import { LoginComponent } from "../auth/login/login.component";
 
 @NgModule({
-  declarations: [PaginationComponent],
+  declarations: [PaginationComponent, LoginComponent],
   imports: [
     CommonModule
   ]
